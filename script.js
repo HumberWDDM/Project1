5111ab256322e9d5037f559a206ee6938341df9f
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2024-10-31 01:11:50 +0100

    Updated CSS and HTML files to reflect change in nav bar colour and scroll functionality

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // add a smooth scroll effect when navigation links are clicked
-
-const navigation = document.querySelectorAll("nav a");
-
-function smoothScroll(event) {
+const navigation = document.querySelectorAll("a");
+function smoothScroll(event) { // listens for mouse click to make scrolling smooth (mouse event 1)
     event.preventDefault();
     const targetId = this.getAttribute("href").substring(1);
     document.getElementById(targetId).scrollIntoView({
@@ -17,9 +15,7 @@ for (let navElement of navigation) {
 
 // Change colour of navigation bar element upon scroll
 const navBar = document.querySelector(".nav_container");
-
-// Function to handle scroll event
-function handleScroll() {
+function handleScroll() { // listens for mouse scroll to  change nav bar colour (mouse event 2)
     if (window.scrollY > 30) {
         navBar.classList.add("scrolled");
     } else {
